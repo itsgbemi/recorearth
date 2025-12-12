d1cbb349abb6e78ec2a5a8470643467a061dd7ee
Gbemisola Oyeniyi <117040187+itsgbemi@users.noreply.github.com>
2025-12-11 16:30:09 -0800

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Hero from './components/Hero'
+import HowItWorks from './components/HowItWorks'
+import ProjectPreview from './components/ProjectPreview'
+import CallToAction from './components/CallToAction'
+
+export default function Home() {
+return (
+<>
+<Hero />
+<HowItWorks />
+<ProjectPreview />
+<CallToAction />
+</>
+)
+}
