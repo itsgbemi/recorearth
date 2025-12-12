7a3c00e2d8981400da0a4284019c0cfccbdf800c
Gbemisola Oyeniyi <117040187+itsgbemi@users.noreply.github.com>
2025-12-11 16:38:15 -0800

    Update Container.tsx

--- app/components/ui/Container.tsx
+++ app/components/ui/Container.tsx
@@ -1,5 +1,4 @@
 import * as React from "react"
-import { cn } from "@/lib/utils"
 
 const Container = React.forwardRef<
 HTMLDivElement,
@@ -7,7 +6,7 @@ React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
 <div
 ref={ref}
-className={cn("mx-auto max-w-7xl px-4 sm:px-6 lg:px-8", className)}
+className={`mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 ${className || ''}`}
 {...props}
 />
 ))
