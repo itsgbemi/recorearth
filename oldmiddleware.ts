16faefedde3cffacaf457aaa117dced00cbfaa4b
Gbemisola Oyeniyi <117040187+itsgbemi@users.noreply.github.com>
2025-12-13 08:50:33 -0800

    Delete oldmiddleware.ts

--- oldmiddleware.ts
+++
@@ -1,9 +0,0 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-export async function middleware(request: NextRequest) {
-const res = NextResponse.next()
-const supabase = createMiddlewareClient({ req: request, res })
-await supabase.auth.getSession()
-return res
-}
