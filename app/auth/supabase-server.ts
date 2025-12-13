e16a59391ca2336e91cce2efdf1e83894bd9248c
Gbemisola Oyeniyi <117040187+itsgbemi@users.noreply.github.com>
2025-12-13 08:45:10 -0800

    Delete app/auth/supabase-server.ts

--- app/auth/supabase-server.ts
+++
@@ -1,5 +0,0 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-export async function createClient() {
-return createServerComponentClient({ cookies })
-}
